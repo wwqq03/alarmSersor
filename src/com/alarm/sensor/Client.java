7462bd9ffe0789189cdc4f05ae04aa93fe763b07
Qi <wwqq03@gmail.com>
2013-11-02 14:49:22 +0100

    Initialize

---
+++ src/com/alarm/sensor/Client.java
@@ -0,0 +1,13 @@
+package com.alarm.sensor;
+
+import hydna.ntnu.student.api.HydnaApi;
+import hydna.ntnu.student.listener.api.HydnaListener;
+
+import aQute.bnd.annotation.component.Activate;
+import aQute.bnd.annotation.component.Component;
+import aQute.bnd.annotation.component.Reference;
+
+public class Client {
+	
+
+}
